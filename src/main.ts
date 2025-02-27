b9df5f6870fae818895c56e59cd74afb56ebe250
nightmare.code.88 <nightmare.code.88@gmail.com>
2025-02-27 14:21:16 -0300

    configuraciones varias maint.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,13 +15,13 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   app.useStaticAssets(join(__dirname, '..', 'node_modules', 'swagger-ui-dist'));
 
-  // Redirigir la raíz a una página de bienvenida
+  // Redirigir la raíz a otra ruta o servir una página vacía
   app.use('/', (req, res) => {
-    res.send('Bienvenido a la API de Películas. Para la documentación de la API, visita /api-docs');
+    res.send('Bienvenido a la API de Películas');
   });
 
   await app.listen(process.env.PORT || 3000);
